9f1923ea1f0617b836b3299db201476b4600c9a1
Loremgreg <gregorybastin.de@gmail.com>
2025-06-23 19:50:22 +0200

    update archive

---
+++ Archive/agent_v1.old.py
@@ -0,0 +1,62 @@
+from dotenv import load_dotenv            
+
+from livekit import agents
+from livekit.agents import (
+    AgentSession,
+    Agent,
+    RoomInputOptions
+)
+from livekit.plugins import (
+    elevenlabs,
+    deepgram,
+    openai,
+    silero,
+    noise_cancellation,
+)
+
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
+
+from prompts import INSTRUCTIONS   
+
+load_dotenv()
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(instructions=INSTRUCTIONS)
+
+
+async def entrypoint(ctx: agents.JobContext):
+    session = AgentSession(
+        stt=deepgram.STT(model="nova-3", language="multi"),
+        llm=openai.LLM(model="gpt-4o-mini"),
+        tts=elevenlabs.TTS(
+            voice_id="FpvROcY4IGWevepmBWO2", 
+            model="eleven_flash_v2_5",
+            language="fr",
+        ),
+        vad=silero.VAD.load(),
+        turn_detection=MultilingualModel(),
+    )
+   
+
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+            # LiveKit Cloud enhanced noise cancellation
+            # - For telephony applications, use `BVCTelephony` for best results
+            noise_cancellation=noise_cancellation.BVC(), 
+        ),
+    )
+
+    await ctx.connect()
+
+    await session.generate_reply(instructions=INSTRUCTIONS)
+
+
+
+
+# Lancement de l’agent vocal (uniquement si le script est exécuté directement)
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
